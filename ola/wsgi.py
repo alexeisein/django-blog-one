eec1d030fb7000672a4d148beb35e5f6bc50480b
alexeisein <alexeis112@gmail.com>
2017-08-13 18:37:15 +0530

    admin url changed

--- ola/wsgi.py
+++ ola/wsgi.py
@@ -14,3 +14,17 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ola.settings")
 
 application = get_wsgi_application()
+
+# FROM PYTHON ANYWHERE
+# import os
+# import sys
+
+# path = os.path.expanduser('~/my-first-blog')
+# if path not in sys.path:
+#     sys.path.append(path)
+
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+# from django.core.wsgi import get_wsgi_application
+# from django.contrib.staticfiles.handlers import StaticFilesHandler
+# application = StaticFilesHandler(get_wsgi_application())
